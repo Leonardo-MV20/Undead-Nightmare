81083958185115b17980b2aee1359abd1a81f7c4
Leonardo-MV20 <leomoscoav20@gmail.com>
2026-07-07 23:01:39 -0600

    Commit con ambientación
    Añadí más detalles al nivel y más movimientos para el personaje.

--- Undead Nightmare/Assets/Scripts/PlayerMoveset.cs
+++ Undead Nightmare/Assets/Scripts/PlayerMoveset.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMoveset : MonoBehaviour
@@ -7,6 +8,7 @@ public class PlayerMoveset : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
     private float movimientoHorizontal;
+    private bool accionBloqueada = false;
 
     void Start()
     {
@@ -16,23 +18,55 @@ public class PlayerMoveset : MonoBehaviour
 
     void Update()
     {
-        movimientoHorizontal = Input.GetAxisRaw("Horizontal");
-
-        bool corriendo = Mathf.Abs(movimientoHorizontal) > 0.01f;
-        animator.SetBool("isRunning", corriendo);
-
-        if (movimientoHorizontal > 0)
+        if (!accionBloqueada)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            movimientoHorizontal = Input.GetAxisRaw("Horizontal");
+
+            bool corriendo = Mathf.Abs(movimientoHorizontal) > 0.01f;
+            animator.SetBool("isRunning", corriendo);
+
+            if (movimientoHorizontal > 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            else if (movimientoHorizontal < 0)
+                transform.localScale = new Vector3(-1, 1, 1);
         }
-        else if (movimientoHorizontal < 0)
+        if (Input.GetKeyDown(KeyCode.J) && !accionBloqueada)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            StartCoroutine(EjecutarAccion("Attack", 0.5f));
+        }
+
+        if (Input.GetKeyDown(KeyCode.K) && !accionBloqueada)
+        {
+            StartCoroutine(EjecutarAccion("Shot", 0.6f));
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && !accionBloqueada)
+        {
+            StartCoroutine(EjecutarAccion("Recharge", 2.0f));
         }
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(movimientoHorizontal * velocidad, rb.velocity.y);
+        if (accionBloqueada)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(movimientoHorizontal * velocidad, rb.velocity.y);
+        }
+    }
+
+    IEnumerator EjecutarAccion(string nombreAnimacion, float duracion)
+    {
+        accionBloqueada = true;
+        movimientoHorizontal = 0;
+        animator.SetBool("isRunning", false);
+        animator.SetTrigger(nombreAnimacion);
+
+        yield return new WaitForSeconds(duracion);
+
+        accionBloqueada = false;
     }
 }
